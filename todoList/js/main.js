97e1ddc27909f16ab3d21df389f800169560038e
ponsima <ponsima1140@gmail.com>
2019-05-17 12:56:16 +0900

    [new] todolist

---
+++ todoList/js/main.js
@@ -0,0 +1,20 @@
+new Vue({
+    el: '#app',
+    data: {
+        newItem: '',
+        todos: []
+    },
+    methods: {
+        addItem: function (event) {
+            // alert();
+            if(this.newItem == '') return;
+            var todo = {
+                item: this.newItem,
+                isDone: false,
+            };
+
+            this.todos.push(todo);
+            this.newItem='';
+        }
+    }
+})
