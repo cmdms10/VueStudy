cfc2724192b856a01cd9cc318c99b5534bba26c2
ponsima <ponsima1140@gmail.com>
2019-05-23 00:27:05 +0900

    [add] コメント追加

--- todoList/js/main.js
+++ todoList/js/main.js
@@ -7,17 +7,24 @@ new Vue({
     methods: {
         addItem: function (event) {
             // alert();
+            //未入力なら追加しない
             if(this.newItem == '') return;
+
             var todo = {
                 item: this.newItem,
+                //タスクの完了/未完了を管理する真偽値を追加
                 isDone: false,
             };
-
+            //配列末尾に追加するメソッド
             this.todos.push(todo);
+
+            //入力後input内を空にする
             this.newItem='';
         },
+        //削除機能追加
         deleteItem: function(index) {
             // alert(index);
+            //該当データの削除
             this.todos.splice(index, 1);
         }
     }
