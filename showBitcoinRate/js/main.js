9806381bca6e621592ef32df1214b0fa14163b01
ponsima <ponsima1140@gmail.com>
2019-05-27 16:10:38 +0900

    [add] 追加

--- showBitcoinRate/js/main.js
+++ showBitcoinRate/js/main.js
@@ -1,19 +1,32 @@
-new Vue ({
+new Vue({
     el: '#app',
     data: {
-        bpi: null
+        bpi: null,
+        hasError: false,
+        loading: true,
     },
+
     mounted: function () {
         axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-        .then(function (response) {
-            // console.log(response.data.bpi)
-            // console.log(response.data.bpi.USD.rate_float)
-            this.bpi = response.data.bpi
-        }.bind(this))
+            .then(function (response) {
+                // console.log(response.data.bpi.USD.rate_float)
+                //プロパティbpiにデータを入れる
+                this.bpi = response.data.bpi
+            }.bind(this))
+            //通信エラーのキャッチ
             .catch(function (error) {
                 console.log(error)
-            })
+                this.hasError = true
+            }.bind(this))
+            //ローディング表示
+            .finally(function () {
+                this.loading = false
+            }.bind(this))
+    },
+    filters: {
+        /*小数点以下の桁数調整*/
+        currencyDecimal(value) {
+            return value.toFixed(2)
+        }
     }
-
-
-});
+})
